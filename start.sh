968488bbe0ef1698e075bd324b3511571c33143e
MeizCrazyLeader <110610631+MeizCrazyLeader@users.noreply.github.com>
2024-01-25 23:42:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/mrmalikoffl/MsFilmFactoryV2
+  git clone https://github.com/MeizCrazyLeader/MsTamil-Filter-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MsFilmFactoryV2
+  git clone $UPSTREAM_REPO /MsTamil-Filter-bot
 fi
-cd /LazyPrincessV2
+cd /MsTamil-Filter-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
